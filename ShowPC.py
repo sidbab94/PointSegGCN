03ed863c8e6a427dcabebb735cc2168936aee66a
Siddharth Baburaj <siddharth.baburaj@gmail.com>
2020-11-05 18:31:55 +0100

    PC vis tryout, .npy

--- ShowPC.py
+++ ShowPC.py
@@ -4,7 +4,6 @@ from vispy.scene import visuals
 import sys
 from numpy import genfromtxt
 
-
 def visualize(pointcloud_array=[], path=''):
     canvas = vispy.scene.SceneCanvas(keys='interactive', show=True)
     view = canvas.central_widget.add_view()
@@ -13,10 +12,18 @@ def visualize(pointcloud_array=[], path=''):
         points = pointcloud_array
     else:
         points = np.fromfile(path, dtype=np.float32).reshape(-1, 4)
+    # point cloud format --> {X, Y, Z, R, G, B, L}
+    if points.shape[1] == 7:
+        pc_colors = points[:, 3:-1] / 255
+        pc_colorsA = (np.ones((pc_colors.shape[0], 4)) * .5)
+        pc_colorsA[:, :-1] = pc_colors
+        vis_RGB = tuple(tuple(row) for row in pc_colorsA)
+    else:
+        vis_RGB = (1, 1, 1, .5)
 
     # create scatter object and fill in the data
     scatter = visuals.Markers()
-    scatter.set_data(points[:, :3], edge_color=None, face_color=(1, 1, 1, .5), size=5)
+    scatter.set_data(points[:, :3], edge_color=None, face_color=vis_RGB, size=5)
 
     view.add(scatter)
     view.camera = 'turntable'  # or try 'arcball'
@@ -27,7 +34,10 @@ def visualize(pointcloud_array=[], path=''):
     if sys.flags.interactive != 1:
         vispy.app.run()
 
-file = 'data/train_data/02691156/000001.pts'
-points = genfromtxt(file, delimiter='')
+
+
+file = 'vkitti3d_01.npy'
+# points = genfromtxt(file, delimiter='')
+points = np.load(file)
 visualize(points)
 
