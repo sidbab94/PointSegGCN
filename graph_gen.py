ae79a96096298ec2f6f2c43628593e0f531b6016
Siddharth Baburaj <siddharth.baburaj@gmail.com>
2020-11-06 19:10:44 +0100

    added kd_tree + sort func

--- graph_gen.py
+++ graph_gen.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy import genfromtxt
 from scipy.spatial.distance import cdist
+from sklearn.neighbors import KDTree
 from time import time
 import networkx as nx
 from mayavi import mlab
@@ -19,6 +20,9 @@ parser.add_option('-v', dest='visualize', action='store_true',
                   help='Enable visualization of graph constructed from input point cloud')
 parser.add_option('-o', dest='optimal', action='store_true',
                   help='Enable optimization of nearest neighbour search algorithm, in favour of performance')
+parser.add_option('-s', dest='sort', action='store_true',
+                  help='Pre-sort points according to viewpoint distance and rgb value')
+
 options, _ = parser.parse_args()
 
 def get_nearest_indices(curr_index, all_points, roi_each=50):
@@ -36,7 +40,6 @@ def get_nearest_indices(curr_index, all_points, roi_each=50):
     target_points = roi_of_point[roi_indices != excl_point_indx, :]
     return target_points
 
-
 def eucl_dist(target_points, source_point):
     """
 
@@ -49,6 +52,17 @@ def eucl_dist(target_points, source_point):
     l2_norm = tf.norm(x - y, ord='euclidean', axis=1)
     print('Next point.')
     return l2_norm
+
+def kdtree(X, nn):
+    tree = KDTree(X)
+    _, nearest_ind = tree.query(X, k=nn+1)
+    return nearest_ind[:, 1:].tolist()
+
+def cuda_tree(X, nn):
+    cuda_tree = CudaTree(X, leaf_size=4)
+    _, cu_indices = cuda_tree.query(X, n_neighbors=nn)
+    cu_indices = cu_indices.tolist()
+    return cu_indices
 
 def get_neighbours_gpu(points, nn):
     """
@@ -104,7 +118,7 @@ def get_neighbours_gpu(points, nn):
 
     return pointpairs
 
-def get_neighbours(points, nn, optim=False):
+def get_neighbours(points, nn, optim=False, tree_search=True):
     """
     TARGET DEVICE - CPU
     :param points: complete point cloud as numpy array
@@ -114,48 +128,50 @@ def get_neighbours(points, nn, optim=False):
     """
     # all indices from 0 to total num_points-1
     indices = np.arange(points.shape[0])
-    # list of all points
-    list_of_points = list(points.tolist())
     # pair of start point and end(neighbour) points in index format
     pointpairs = []
 
-    for point in enumerate(points):
-        # current point index
-        curr_point_indx = point[0]
-        # current point coordinates in np array format
-        curr_point = np.array([point[1]])
+    if tree_search:
+        endpoint_indices = kdtree(points, nn)
+        pointpairs = [list(i) for i in zip(indices, endpoint_indices)]
+    else:
+        list_of_points = list(points.tolist())
+        for point in enumerate(points):
+            # current point index
+            curr_point_indx = point[0]
+            # current point coordinates in np array format
+            curr_point = np.array([point[1]])
 
-        # all points except current point
-        if optim:
-            targets = get_nearest_indices(curr_point_indx, points, roi_each=100)
-        else:
-            targets = points[indices != curr_point_indx, :]
+            # all points except current point
+            if optim:
+                targets = get_nearest_indices(curr_point_indx, points, roi_each=100)
+            else:
+                targets = points[indices != curr_point_indx, :]
 
-        # Compute euclidean distance between points using scipy
-        distances = cdist(targets, curr_point, metric='sqeuclidean').flatten().tolist()
+            # Compute euclidean distance between points using scipy
+            distances = cdist(targets, curr_point, metric='sqeuclidean').flatten().tolist()
 
-        # index -> coordinates map for target points
-        target_indx_dict = dict(enumerate(targets))
-        # index -> distances map
-        dist_indx_dict = dict(enumerate(distances))
+            # index -> coordinates map for target points
+            target_indx_dict = dict(enumerate(targets))
+            # index -> distances map
+            dist_indx_dict = dict(enumerate(distances))
 
-        # sorted distances in ascending order
-        sorted_distances = {k: v for k, v in sorted(dist_indx_dict.items(), key=lambda item: item[1])}
-        # nearest neighbour indices
-        nn_indices = list(sorted_distances.keys())[:nn]
-        assert len(nn_indices) == nn
-        # nearest points:       list
-        nearest_points = list(target_indx_dict[i].tolist() for i in nn_indices)
-        assert len(nearest_points) == nn
-        # current point
-        start = point[1].flatten()
-        # current point global index
-        startpoint_index = list_of_points.index(start.tolist())
-        # end point(s) global indices
-        endpoint_indices = [list_of_points.index(nearest_points[i]) for i in range(len(nearest_points))]
-        # start-end point global index pairs
-        pointpairs.extend([[startpoint_index, endpoint_indices]])
-
+            # sorted distances in ascending order
+            sorted_distances = {k: v for k, v in sorted(dist_indx_dict.items(), key=lambda item: item[1])}
+            # nearest neighbour indices
+            nn_indices = list(sorted_distances.keys())[:nn]
+            assert len(nn_indices) == nn
+            # nearest points:       list
+            nearest_points = list(target_indx_dict[i].tolist() for i in nn_indices)
+            assert len(nearest_points) == nn
+            # current point
+            start = point[1].flatten()
+            # current point global index
+            startpoint_index = list_of_points.index(start.tolist())
+            # end point(s) global indices
+            endpoint_indices = [list_of_points.index(nearest_points[i]) for i in range(len(nearest_points))]
+            # start-end point global index pairs
+            pointpairs.extend([[startpoint_index, endpoint_indices]])
     return pointpairs
 
 class Graph(object):
@@ -163,10 +179,14 @@ class Graph(object):
     Graph constructor class
     """
     def __init__(self, numNodes):
-        self.adjacencyMatrix = []  # 2D list
-        for i in range(numNodes):
-            self.adjacencyMatrix.append([0 for i in range(numNodes)])
+        # self.adjacencyMatrix = []  # 2D list
+        # for i in range(numNodes):
+        #     self.adjacencyMatrix.append([0 for i in range(numNodes)])
+        # self.numNodes = numNodes
+
+        self.adjacencyMatrix = np.empty((numNodes, numNodes), dtype='uint8')
         self.numNodes = numNodes
+
 
     def addEdge(self, start, end):
         """
@@ -175,16 +195,16 @@ class Graph(object):
         :param end: End node global index
         :return: Modified adjacency matrix
         """
-        self.adjacencyMatrix[start][end] = 1
+        self.adjacencyMatrix[start, end] = 1
 
     def removeEdge(self, start, end):
-        if self.adjacencyMatrix[start][end] == 0:
+        if self.adjacencyMatrix[start, end] == 0:
             print("There is no edge between %d and %d" % (start, end))
         else:
-            self.adjacencyMatrix[start][end] = 0
+            self.adjacencyMatrix[start, end] = 0
 
     def containsEdge(self, start, end):
-        if self.adjacencyMatrix[start][end] > 0:
+        if self.adjacencyMatrix[start, end] > 0:
             return True
         else:
             return False
@@ -213,7 +233,6 @@ def rgb_2_scalar_idx(r, g, b):
     :return: scalar index of input colour
     """
     return 256**2 *r + 256 * g + b
-
 
 def visualize_graph_rgb(graph, array):
     """
@@ -250,7 +269,7 @@ def visualize_graph_rgb(graph, array):
     pts.module_manager.scalar_lut_manager.lut.number_of_colors = rgb_lut.shape[0]
     pts.module_manager.scalar_lut_manager.lut.table = rgb_lut
     pts.glyph.scale_mode = 'data_scaling_off'
-    pts.glyph.glyph.clamping = False
+    # pts.glyph.glyph.clamping = False
 
     mlab.show()
 
@@ -276,6 +295,24 @@ def visualize_graph_xyz(graph, array):
     pts.glyph.scale_mode = 'data_scaling_off'
     mlab.show()
 
+def sort_pc(points):
+    points = points[np.random.choice(points.shape[0],
+                                     round(0.07 * points.shape[0]),
+                                     replace=False), :]
+    x_sorted = points[np.argsort(points[:, 0])]
+    x_sorted = x_sorted[:30000, :]
+    pc_xyz = x_sorted[:, :3]
+    pc_reform = np.empty((pc_xyz.shape[0], 4))
+    pc_reform[:, :3] = pc_xyz
+    pc_rgb = points[:, 3:-1]
+    for irgb in enumerate(pc_rgb[:10]):
+        color = irgb[1]
+        indx = irgb[0]
+        scalar = rgb_2_scalar_idx(color[0], color[1], color[2])
+        pc_reform[indx, 3] = scalar
+    sorted = x_sorted[np.argsort(pc_reform[:, 3])]
+    return sorted
+
 
 np.random.seed(89)
 
@@ -289,14 +326,19 @@ elif input_cloud_fmt == 'npy':
 else:
     raise Exception('Provide a supported point cloud format')
 N = pc_array.shape[0]
-sample_size = round(options.ss * 0.01 * N)
 NN = options.nearestN
+sample_size = round(options.ss * 0.01 * N)
 pc_array = pc_array[np.random.choice(N, sample_size, replace=False), :]
+if options.sort:
+    pc_array = sort_pc(pc_array)
 print('Total number of sampled points: ', pc_array.shape[0])
 pc_array_XYZ = pc_array[:, :3]
 
 points_dict = dict(enumerate(pc_array_XYZ.tolist()))
+G_start = time()
 G = Graph(numNodes=pc_array.shape[0])
+
+print('Graph construction elapsed time: ', time() - G_start)
 
 if options.gpu_en:
     device = 'GPU'
@@ -308,7 +350,7 @@ else:
     device = 'CPU'
     start = time()
     index_pairs = get_neighbours(pc_array_XYZ, NN, optim=options.optimal)
-print('Time elapsed in seconds for {} points: {}, using {}'.format(pc_array.shape[0], time()-start, device))
+print('NNS: Time elapsed in seconds for {} points: {}, using {}'.format(pc_array.shape[0], time()-start, device))
 
 for pair in index_pairs:
     startindex = pair[0]
