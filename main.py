a1d018ed282af07ee90e8106033bacf7027970bf
Siddharth Baburaj <siddharth.baburaj@ipm.fraunhofer.de>
2020-11-18 16:59:28 +0100

    minor clean-up

--- main.py
+++ main.py
@@ -7,42 +7,52 @@ from visualization import show_voxel
 from optparse import OptionParser
 from mayavi import mlab
 
-
 parser = OptionParser()
 parser.add_option('--input', dest='inputpc', default='samples/vkitti3d_01.npy',
-                  help='Provide input point cloud file in .pts format')
+                  help='Provide input point cloud file in supported format (.pts, .csv, .npy) '
+                       '** DEFAULT: samples/vkitti3d_01.npy')
 parser.add_option('--NN', dest='nearestN', default=2, type='int',
-                  help='Provide number of nearest neighbours to process each point with')
-parser.add_option('--sample_size', dest='ss', default=50.0, type='float',
-                  help='Provide proportion of points (in %) to be randomly sampled from input distribution')
+                  help='Provide number of nearest neighbours to process each point with. '
+                       ' ** DEFAULT: 2')
+parser.add_option('--sample_size', dest='ss', default=100.0, type='float',
+                  help='Provide proportion of points (in %) to be randomly sampled from input. '
+                       ' ** DEFAULT: 50.0')
 parser.add_option('--nns_method', dest='searchmethod', default='kdtree',
-                  help='Specify method to implement nearest neighbour search -- knn or kdtree?')
+                  help='Specify method to implement nearest neighbour search -- knn or kdtree? '
+                       ' ** DEFAULT: kdtree')
+parser.add_option('--vox_factor', dest='div_factor', default=15, type='int',
+                  help='Specify voxelization factor (number of voxels along principal axis) '
+                       ' ** DEFAULT: 10')
+parser.add_option('--vox_thresh', dest='occ_thresh', default=5e-3, type='float',
+                  help='Specify threshold as factor of original point cloud size, below which voxels would be ignored'
+                       ' ** DEFAULT: 9e-3')
 parser.add_option('-v', dest='visualize', action='store_true',
                   help='Enable visualization of graph constructed from input point cloud')
 parser.add_option('-o', dest='optimal', action='store_true',
                   help='Enable optimization of nearest neighbour search algorithm, by pre-sorting points')
-parser.add_option('--vox_factor', dest='div_factor', default=10, type='int',
-                  help='Specify voxelization factor (number of voxels along principal axis)')
-parser.add_option('--vox_thresh', dest='occ_thresh', default=9e-3, type='float',
-                  help='Specify threshold as factor of original point cloud size, below which voxels would be ignored')
 
 options, _ = parser.parse_args()
 np.random.seed(89)
 
 
-
-def construct_vox_graph(vox_pc_map, visual=False):
+def construct_vox_graph(vox_pc_map, N, visual=False):
+    elapsed = 0.0
     for vox_id in range(len(vox_pc_map)):
-
+        vox_start = time()
         vox_pts = vox_pc_map[vox_id]
         nns = graph.NearestNodeSearch(pointcloud=vox_pts, options=options)
         nns.get_neighbours()
         G = nns.graph
-
+        elapsed = (time() - vox_start) + elapsed
+        if vox_id == len(vox_pc_map) - 1:
+            print('     Graph construction done.')
         if visual:
             show_voxel(vox_pts, G)
     if visual:
+        print('     Visualizing...')
         mlab.show()
+    print('----------------------------------------------------------------------------------')
+    print('Time elapsed for {} voxel(s) and {} points: {} seconds'.format(len(vox_pc_map), N, round(elapsed, 5)))
 
 
 def main():
@@ -66,6 +76,7 @@ def main():
     print('==================================================================================')
     print('----------------------------------------------------------------------------------')
     print('1.   Performing voxelization...')
+    print(pc.shape)
     grid = vox.voxelize(pc, options.div_factor)
     grid.get_voxels()
     print('     Voxelization done.')
@@ -73,11 +84,7 @@ def main():
     visual = options.visualize
     print('----------------------------------------------------------------------------------')
     print('2.   Constructing graphs for voxels...')
-    vox_start = time()
-    construct_vox_graph(vox_pc_map, visual=visual)
-    print('     Graph construction done.')
-    print('----------------------------------------------------------------------------------')
-    print('Time elapsed for {} voxel(s) and {} points: {} s'.format(len(vox_pc_map)+1, pc.shape[0], time() - vox_start))
+    construct_vox_graph(vox_pc_map, N=pc.shape[0], visual=visual)
     print('----------------------------------------------------------------------------------')
     print('==================================================================================')
 
