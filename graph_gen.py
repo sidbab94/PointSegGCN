191a047dc080b63648d6383708ad4a9fa761113a
Siddharth Baburaj <siddharth.baburaj@ipm.fraunhofer.de>
2020-12-02 14:23:04 +0100

    added sklearn kd_tree graph func (opt)

--- graph_gen.py
+++ graph_gen.py
@@ -1,9 +1,9 @@
 import numpy as np
 from scipy.spatial.distance import cdist
 from sklearn.neighbors import NearestNeighbors
+from sklearn.neighbors import KDTree
 import networkx as nx
 import scipy
-
 
 def sortpoints(points):
     x_sorted = points[np.argsort(points[:, 0])]
@@ -16,12 +16,38 @@ def sortpoints(points):
     return points
 
 def kdtree(points, nn):
-    search = NearestNeighbors(n_neighbors=nn, algorithm='kd_tree').fit(points)
+    search = NearestNeighbors(n_neighbors=nn+1, algorithm='kd_tree').fit(points)
     dist, idx = search.kneighbors(points)
-    return dist, idx
+    return dist[:, 1:], idx[:, 1:]
+
+def kdgraph(points, nn):
+    search = NearestNeighbors(n_neighbors=nn+1, algorithm='kd_tree').fit(points[:, :3])
+    adj = search.kneighbors_graph(points[:, :3], mode='distance')
+    return adj
+
+def show(pts, graph):
+    point_size = 0.005
+    edge_size = 0.001
+    G = nx.convert_node_labels_to_integers(graph)
+    scalars = np.array(list(G.nodes())) + 5
+    pts = mlab.points3d(
+        pts[:, 0],
+        pts[:, 1],
+        pts[:, 2],
+        scalars,
+        scale_factor=point_size,
+        scale_mode="none",
+        colormap="Blues",
+        resolution=20)
+    pts.mlab_source.dataset.lines = np.array(list(G.edges()))
+    tube = mlab.pipeline.tube(pts, tube_radius=edge_size)
+    mlab.pipeline.surface(tube, color=(0.8, 0.8, 0.8))
+    pts.glyph.scale_mode = 'data_scaling_off'
+
 
 def adjacency(points, nn):
-    dist, idx = kdtree(points, nn)
+    dist, idx = kdtree(points[:, :3], nn)
+    # print(idx.shape, type(idx))
     M, k = dist.shape
     assert M, k == idx.shape
     assert dist.min() >= 0
@@ -46,6 +72,21 @@ def adjacency(points, nn):
     assert type(W) is scipy.sparse.csr.csr_matrix
     return nx.from_scipy_sparse_matrix(W)
 
+# from mayavi import mlab
+# import voxelization as vox
+# from visualization import show_voxel
+# pc = np.genfromtxt('samples/airplane.pts', delimiter='')
+# # g = adjacency(pc, 2)
+# grid = vox.voxelize(pc, 15)
+# grid.get_voxels()
+# vox_pc_map = grid.voxel_points
+# for vox_id in range(len(vox_pc_map)):
+#     vox_pts = vox_pc_map[vox_id]
+#     g = adjacency(vox_pts, 2)
+#     show_voxel(vox_pts, g, vis_scale=0)
+# mlab.show()
+
+
 class NearestNodeSearch:
     def __init__(self, pointcloud, options):
         self.points_alldims = pointcloud
@@ -58,6 +99,7 @@ class NearestNodeSearch:
     def kdtree(self):
         search = NearestNeighbors(n_neighbors=self.nn + 1, algorithm='kd_tree').fit(self.points)
         _, nearest_ind = search.kneighbors(self.points)
+        print(nearest_ind[:, 1:].shape)
         return nearest_ind[:, 1:].tolist()
 
     def get_neighbours(self):
@@ -65,6 +107,7 @@ class NearestNodeSearch:
 
         if self.nnsmode == 'kdtree':
             endpoint_indices = self.kdtree()
+            # print(endpoint_indices.shape, type(endpoint_indices))
             nn_indices = [list(i) for i in zip(indices, endpoint_indices)]
             for pair in nn_indices:
                 i = pair[0]
