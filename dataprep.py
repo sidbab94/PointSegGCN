b8518338d82951c2459c01ad31068efc62011f91
Siddharth Baburaj <siddharth.baburaj@gmail.com>
2020-12-08 11:17:06 +0100

    moved preproc funcs here

--- dataprep.py
+++ dataprep.py
@@ -2,8 +2,9 @@ import numpy as np
 import os
 import yaml
 from os.path import join
+import struct
 
-BASE_DIR = 'D:/SemanticKITTI/dataset/sequences'
+# BASE_DIR = 'D:/SemanticKITTI/dataset/sequences'
 
 def get_split_files(dataset_path, config_file='semantic-kitti.yaml'):
 
@@ -36,5 +37,32 @@ def get_split_files(dataset_path, config_file='semantic-kitti.yaml'):
 
     return train_file_list, val_file_list, test_file_list
 
+def read_bin_velodyne(path):
+    pc_list = []
+    with open(path, 'rb') as f:
+        content = f.read()
+        pc_iter = struct.iter_unpack('ffff', content)
+        for idx, point in enumerate(pc_iter):
+            pc_list.append([point[0], point[1], point[2]])
+    return np.asarray(pc_list, dtype=np.float32)
 
+def load_label_kitti(label_path, remap_lut):
+    label = np.fromfile(label_path, dtype=np.uint32)
+    label = label.reshape((-1))
+    sem_label = label & 0xFFFF  # semantic label in lower half
+    inst_label = label >> 16  # instance id in upper half
+    assert ((sem_label + (inst_label << 16) == label).all())
+    sem_label = remap_lut[sem_label]
+    return sem_label.astype(np.int32)
+
+def get_labels(label_path):
+    assert os.path.isfile('semantic-kitti.yaml')
+    # label mapping with semantic-kitti config file
+    DATA = yaml.safe_load(open('semantic-kitti.yaml', 'r'))
+    remap_dict_val = DATA["learning_map"]
+    max_key = max(remap_dict_val.keys())
+    remap_lut_val = np.zeros((max_key + 100), dtype=np.int32)
+    remap_lut_val[list(remap_dict_val.keys())] = list(remap_dict_val.values())
+    labels = load_label_kitti(label_path, remap_lut=remap_lut_val)
+    return labels
 
