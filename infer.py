ded1e5c85d4b8dfa5fd949a13cc8493c562dfc3e
Siddharth Baburaj <siddharth.baburaj@gmail.com>
2021-01-08 13:33:34 +0100

    added preproc pipeline

--- infer.py
+++ infer.py
@@ -1,29 +1,24 @@
 import os
 from pathlib import Path
-import numpy as np
-import random
 import tensorflow as tf
 from tensorflow.keras.models import load_model
-from yaml import safe_load
+import random
 
-from preproc_utils.dataprep import get_split_files, get_cfg_params
-from batch_gen import process_single
 from train_utils.eval_metrics import iouEval
-from visualization import ShowPC
-from vis_aux.visualize import ScanVis
 from model import res_model_1 as Net
 
+from preprocess import *
+from visualization import PC_Vis
 
-def test_single(test_file, loaded_model, cfg):
-    x, a, y = process_single(test_file, cfg)
+def test_single(test_file, loaded_model, cfg, prep_obj):
+    x, a, y = prep_obj.assess_scan(test_file)
 
-    # predictions = loaded_model([x, a], training=False)
     predictions = loaded_model.predict_step([x, a])
     pred_labels = np.argmax(predictions, axis=-1)
 
     print(np.unique(y), np.unique(pred_labels))
 
-    class_ignore = cfg["learning_ignore"]
+    class_ignore = cfg["class_ignore"]
     test_miou = iouEval(len(class_ignore), class_ignore)
 
     test_miou.addBatch(pred_labels, y)
@@ -31,22 +26,23 @@ def test_single(test_file, loaded_model, cfg):
     print('Mean IoU: ', te_miou * 100)
     print('IoU: ', iou * 100)
 
-    # ShowPC.draw_pc_sem_ins(pc_xyz=x[:, :3], pc_sem_ins=pred_labels)
-    vis_obj = ScanVis(config=cfg, scan_file=test_file, orig_labels=y, pred_labels=pred_labels)
-    vis_obj.run()
+    PC_Vis.eval(pc=x, y_true=y, cfg=cfg, y_pred=pred_labels)
+
 
 if __name__ == '__main__':
     BASE_DIR = 'D:/SemanticKITTI/dataset/sequences'
 
-    load_frm_model = False
-    cfg = safe_load(open('config/semantic-kitti.yaml', 'r'))
+    load_frm_model = True
+    # cfg = safe_load(open('config/semantic-kitti.yaml', 'r'))
 
-    tr_dict = get_cfg_params(base_dir=BASE_DIR)
+    cfg = get_cfg_params(base_dir=BASE_DIR)
 
-    train_files, val_files, test_files = get_split_files(dataset_path=BASE_DIR, cfg=tr_dict, count=5)
-    # test_file = random.choice(val_files)
-    test_file = val_files[0]
+    train_files, val_files, test_files = get_split_files(dataset_path=BASE_DIR, cfg=cfg)
+    test_file = random.choice(val_files)
+    # test_file = val_files[0]
     print(test_file)
+
+    prep = Preprocess(cfg)
 
     if load_frm_model:
         latest_model_path = sorted(Path('./models').iterdir(), key=os.path.getmtime)[-1]
@@ -54,14 +50,14 @@ if __name__ == '__main__':
         loaded_model = load_model(filepath=latest_model_path, compile=False)
 
     else:
-        loaded_model = Net(tr_dict)
+        loaded_model = Net(cfg)
         latest_checkpoint = tf.train.latest_checkpoint('./ckpt_weights')
         print(latest_checkpoint)
         load_status = loaded_model.load_weights('./ckpt_weights/2021-01-04--06.08.13')
         # load_status.assert_consumed()
         print(loaded_model)
 
-    test_single(test_file, loaded_model, cfg)
+    test_single(test_file, loaded_model, cfg, prep)
 
 
 
--- train.py
+++ train.py
@@ -1,91 +1,71 @@
-import numpy as np
 import datetime
-import tensorflow as tf
-import sys
 
+import tensorflow as tf
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.optimizers.schedules import ExponentialDecay
 
-from preproc_utils.dataprep import get_split_files, get_cfg_params
-from train_utils.lovasz_loss import lovasz_softmax_flat
-from batch_gen import process_single, process_voxel, voxel
+from train_utils.loss_metrics import lovasz_softmax_flat
 from train_utils.eval_metrics import iouEval
-from model import Graph_U as network
-
-
-def get_apply_grad_fn():
-    @tf.function
-    def apply_grad(inputs, target):
-        with tf.GradientTape() as t:
-
-            predictions = model(inputs)
-
-            loss = lovasz_softmax_flat(predictions, target)
-
-        grads = t.gradient(loss, model.trainable_weights)
-
-        opt.apply_gradients(zip(grads, model.trainable_weights))
-
-        return predictions, loss
-    return apply_grad
-
+from model import res_model_2 as network
+from preprocess import *
 
 # @tf.function(input_signature=[[tf.TensorSpec(shape=tf.TensorShape(None), dtype=tf.float32),
 #                                tf.SparseTensorSpec(shape=tf.TensorShape(None), dtype=tf.float32)],
 #              tf.TensorSpec(shape=tf.TensorShape(None), dtype=tf.int32)])
 # @tf.function
-def grad_func(inputs, target):
+
+def grad_func(inputs, target, loss_fn):
     with tf.GradientTape() as tape:
         predictions = model(inputs, training=True)
-        curr_tr_loss = loss_cross_entropy(target, predictions)
+        curr_tr_loss = loss_fn(target, predictions)
 
     gradients = tape.gradient(curr_tr_loss, model.trainable_variables)
     opt.apply_gradients(zip(gradients, model.trainable_variables))
 
     return predictions, curr_tr_loss
 
-def train_step(batch, lovasz=False, vox=False):
+def train_step(batch, prep_obj, lovasz=False, vox=False):
 
     inputs = [batch[0], batch[1]]
     target = batch[2]
-    train_miou.reset_states()
+
+    loss_fn = loss_cross_entropy
+    # train_miou.reset_states()
+    if lovasz:
+        loss_fn = lovasz_softmax_flat
 
     if vox:
-        vox_pc_map = voxel(batch[0])
+        prep_obj.voxelize_scan()
+        no_voxels = len(prep_obj.vox_pc_map)
+
         tr_loss = tr_miou = 0
 
-        for vox_id in range(len(vox_pc_map)):
-            curr_voxel_data = process_voxel(vox_pc_map, vox_id, target)
+        for vox_id in range(no_voxels):
+
+            curr_voxel_data = prep_obj.assess_voxel(vox_id)
 
             vox_inputs = [curr_voxel_data[0], curr_voxel_data[1]]
             vox_target = curr_voxel_data[2]
 
-            predictions, curr_tr_loss = grad_func(vox_inputs, vox_target)
+            predictions, curr_tr_loss = grad_func(vox_inputs, vox_target, loss_fn)
 
-            # pred_labels = np.argmax(predictions, axis=-1)
-            pred_labels = tf.argmax(predictions, axis=-1)
+            pred_labels = np.argmax(predictions, axis=-1)
+            # pred_labels = tf.argmax(predictions, axis=-1)
 
-            # train_miou.addBatch(pred_labels, vox_target)
-            # curr_tr_miou, _ = train_miou.getIoU()
+            train_miou.addBatch(pred_labels, vox_target)
+            curr_tr_miou, _ = train_miou.getIoU()
 
-            train_miou.update_state(vox_target, pred_labels)
+            # train_miou.update_state(vox_target, pred_labels)
 
             tr_loss += curr_tr_loss
-            # tr_miou += curr_tr_miou
-            tr_miou += train_miou.result()
+            tr_miou += curr_tr_miou
+            # tr_miou += train_miou.result()
 
-        tr_loss, tr_miou = tr_loss / (len(vox_pc_map)), tr_miou / (len(vox_pc_map))
+        tr_loss, tr_miou = tr_loss / no_voxels, tr_miou / no_voxels
 
     else:
-        with tf.GradientTape() as tape:
-            predictions = model(inputs, training=True)
-            if lovasz:
-                tr_loss = lovasz_softmax_flat(predictions, target)
-            else:
-                tr_loss = loss_cross_entropy(target, predictions)
 
-        gradients = tape.gradient(tr_loss, model.trainable_variables)
-        opt.apply_gradients(zip(gradients, model.trainable_variables))
+        predictions, tr_loss = grad_func(inputs, target, loss_fn)
 
         pred_labels = np.argmax(predictions, axis=-1)
         train_miou.addBatch(pred_labels, target)
@@ -93,46 +73,50 @@ def train_step(batch, lovasz=False, vox=False):
 
     return tr_loss, tr_miou
 
-def evaluate(batch, lovasz=False, vox=False):
+def evaluate(batch, prep_obj, lovasz=False, vox=False):
 
     inputs = [batch[0], batch[1]]
     target = batch[2]
-    val_miou.reset_states()
 
+    loss_fn = loss_cross_entropy
+    # val_miou.reset_states()
+
+    if lovasz:
+        loss_fn = lovasz_softmax_flat
 
     if vox:
-        vox_pc_map = voxel(batch[0])
+        prep_obj.voxelize_scan()
+        no_voxels = len(prep_obj.vox_pc_map)
+
         va_loss = va_miou = 0
 
-        for vox_id in range(len(vox_pc_map)):
-            curr_voxel_data = process_voxel(vox_pc_map, vox_id, target)
+        for vox_id in range(no_voxels):
+
+            curr_voxel_data = prep_obj.assess_voxel(vox_id)
 
             vox_inputs = [curr_voxel_data[0], curr_voxel_data[1]]
             vox_target = curr_voxel_data[2]
 
             predictions = model(vox_inputs, training=False)
 
-            if lovasz:
-                curr_va_loss = lovasz_softmax_flat(predictions, vox_target)
-            else:
-                curr_va_loss = loss_cross_entropy(vox_target, predictions)
+            curr_va_loss = loss_fn(vox_target, predictions)
 
             pred_labels = np.argmax(predictions, axis=-1)
 
-            # val_miou.addBatch(pred_labels, vox_target)
-            # curr_va_miou, _ = val_miou.getIoU()
-            val_miou.update_state(vox_target, pred_labels)
+            val_miou.addBatch(pred_labels, vox_target)
+            curr_va_miou, _ = val_miou.getIoU()
+            # val_miou.update_state(vox_target, pred_labels)
 
             va_loss += curr_va_loss
-            va_miou += val_miou.result()
+            # va_miou += val_miou.result()
+            va_miou += curr_va_miou
 
-        va_loss, va_miou = va_loss / (len(vox_pc_map)), va_miou / (len(vox_pc_map))
+        va_loss, va_miou = va_loss / no_voxels, va_miou / no_voxels
+
     else:
         predictions = model(inputs, training=False)
-        if lovasz:
-            va_loss = lovasz_softmax_flat(predictions, target)
-        else:
-            va_loss = loss_cross_entropy(target, predictions)
+
+        va_loss = loss_fn(target, predictions)
 
         pred_labels = np.argmax(predictions, axis=-1)
         val_miou.addBatch(pred_labels, target)
@@ -140,24 +124,25 @@ def evaluate(batch, lovasz=False, vox=False):
 
     return va_loss, va_miou
 
-@tf.function
-@tf.autograph.experimental.do_not_convert
-def train_ckpt_loop(train_cfg, restore_ckpt=False, verbose=True, vox=True, save_weights=False):
+# @tf.function
+# @tf.autograph.experimental.do_not_convert
+def train_ckpt_loop(model_cfg, prep_obj, restore_ckpt=False, verbose=False, vox=False, save_weights=True):
     if restore_ckpt:
         ckpt.restore(manager.latest_checkpoint)
         print("Restored from {}".format(manager.latest_checkpoint))
 
     best_va_miou = 0.0
+    best_va_loss = 99999
 
-    EPOCHS = train_cfg['ep']
-    loss_switch_ep = train_cfg['loss_switch_ep']
+    EPOCHS = model_cfg['ep']
+    loss_switch_ep = model_cfg['loss_switch_ep']
     lovasz = False
 
     for epoch in range(EPOCHS):
 
         tr_loss = tr_miou = va_loss = va_miou = 0.0
 
-        if epoch == loss_switch_ep:
+        if (epoch + 1) == loss_switch_ep:
             lovasz = True
             print('//////////////////////////////////////////////////////////////////////////////////')
             print('Switching loss function to Lovàsz-Softmax..')
@@ -166,13 +151,12 @@ def train_ckpt_loop(train_cfg, restore_ckpt=False, verbose=True, vox=True, save_
         for tr_file in train_files:
             if verbose:
                 print('     --> Processing train file: ', tr_file)
-            tr_inputs = process_single(tr_file, train_cfg, verbose)
-            tr_outs = train_step(tr_inputs, lovasz, vox=vox)
+            tr_inputs = prep_obj.assess_scan(tr_file)
+            tr_outs = train_step(tr_inputs, prep_obj, lovasz, vox=vox)
             tr_loss += tr_outs[0]
             tr_miou += tr_outs[1]
             if verbose:
-                print(tr_loss.numpy())
-                tf.print("MIOU: ", train_miou.result(), output_stream=sys.stdout)
+                print('         Current Train mIoU: ', round(tr_miou*100))
 
         tr_loss, tr_miou = tr_loss / (len(train_files)), tr_miou / (len(train_files))
 
@@ -188,12 +172,12 @@ def train_ckpt_loop(train_cfg, restore_ckpt=False, verbose=True, vox=True, save_
         for va_file in val_files:
             if verbose:
                 print('     --> Processing valid file: ', va_file)
-            va_inputs = process_single(va_file, train_cfg, verbose)
-            va_outs = evaluate(va_inputs, lovasz, vox=vox)
+            va_inputs = prep_obj.assess_scan(va_file)
+            va_outs = evaluate(va_inputs, prep_obj, lovasz, vox=vox)
             va_loss += va_outs[0]
             va_miou += va_outs[1]
             if verbose:
-                print('     --> Current Valid mIoU: ', round(va_miou * 100))
+                print('         Current Valid mIoU: ', round(va_miou * 100))
 
         va_loss, va_miou = va_loss / (len(val_files)), va_miou / (len(val_files))
 
@@ -209,38 +193,46 @@ def train_ckpt_loop(train_cfg, restore_ckpt=False, verbose=True, vox=True, save_
                       va_loss, va_miou * 100))
 
         ckpt.step.assign_add(1)
-        if (int(ckpt.step) % 5 == 0) and (va_miou > best_va_miou):
+
+        if (int(ckpt.step) % 2 == 0):
+
             print('----------------------------------------------------------------------------------')
+
             save_path = manager.save()
             weights_path = './ckpt_weights/' + current_time
             print("Saved checkpoint for step {}: {}".format(int(ckpt.step), save_path))
-            if save_weights:
+
+            if (va_miou > best_va_miou) and (va_loss < best_va_loss) and save_weights:
                 print("Saved weights for step {}: {}".format(int(ckpt.step), weights_path))
-                model.save_weights(weights_path, overwrite=False)
-            best_va_miou = va_miou
+                model.save_weights(weights_path, overwrite=True)
+                best_va_miou = va_miou
+                best_va_loss = va_loss
+
             print('----------------------------------------------------------------------------------')
 
 
 if __name__ == '__main__':
     BASE_DIR = 'D:/SemanticKITTI/dataset/sequences'
 
-    train_cfg = get_cfg_params(base_dir=BASE_DIR)
+    model_cfg = get_cfg_params(base_dir=BASE_DIR)
 
     TRIAL = True
 
     if TRIAL:
-        train_files, val_files, _ = get_split_files(dataset_path=BASE_DIR, cfg=train_cfg, shuffle=True, count=1)
+        train_files, val_files, _ = get_split_files(dataset_path=BASE_DIR, cfg=model_cfg, shuffle=True, count=50)
     else:
-        train_files, val_files, _ = get_split_files(dataset_path=BASE_DIR, cfg=train_cfg, shuffle=True)
+        train_files, val_files, _ = get_split_files(dataset_path=BASE_DIR, cfg=model_cfg, shuffle=True)
 
-    class_ignore = train_cfg["class_ignore"]
+    class_ignore = model_cfg["class_ignore"]
 
-    model = network(train_cfg)
+    prep = Preprocess(model_cfg)
+
+    model = network(model_cfg)
     # model.summary()
 
     lr_schedule = ExponentialDecay(
-        train_cfg['learning_rate'],
-        decay_steps=train_cfg['lr_decay'],
+        model_cfg['learning_rate'],
+        decay_steps=model_cfg['lr_decay'],
         decay_rate=0.96,
         staircase=True)
     opt = Adam(learning_rate=lr_schedule)
@@ -257,23 +249,24 @@ if __name__ == '__main__':
     valid_summary_writer = tf.summary.create_file_writer(valid_log_dir)
     summary_writer = tf.summary.create_file_writer(gen_log_dir)
 
-    num_classes = train_cfg['num_classes']
-    patience = train_cfg['patience']
+    num_classes = model_cfg['num_classes']
+    patience = model_cfg['patience']
 
-    # train_miou = iouEval(num_classes, class_ignore)
-    # val_miou = iouEval(num_classes, class_ignore)
-    train_miou = tf.keras.metrics.MeanIoU(num_classes)
-    val_miou = tf.keras.metrics.MeanIoU(num_classes)
+    train_miou = iouEval(num_classes, class_ignore)
+    val_miou = iouEval(num_classes, class_ignore)
+    # mean_loss_values = tf.metrics.Mean()
+    # train_miou = tf.keras.metrics.MeanIoU(num_classes)
+    # val_miou = tf.keras.metrics.MeanIoU(num_classes)
 
     print('     TRAINING START...')
     print('----------------------------------------------------------------------------------')
 
-    train_ckpt_loop(train_cfg)
+    train_ckpt_loop(model_cfg, prep, vox=False)
 
     print('----------------------------------------------------------------------------------')
     print('     TRAINING END...')
 
-    save_path = 'models/infer_v1_9_U_cheb1'
+    save_path = 'models/infer_v2_1_res_rgb'
     model.save(save_path)
     print('     Model saved to {}'.format(save_path))
     print('==================================================================================')
