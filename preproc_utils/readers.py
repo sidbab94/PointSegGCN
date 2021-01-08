2483cae946de583f128617e67770034ab95fdfc7
Siddharth Baburaj <siddharth.baburaj@gmail.com>
2021-01-08 13:35:56 +0100

    scan data read helper funcs

---
+++ preproc_utils/readers.py
@@ -0,0 +1,48 @@
+import numpy as np
+import struct
+
+def read_bin_velodyne(pc_path):
+    pc_list = []
+    with open(pc_path, 'rb') as f:
+        content = f.read()
+        pc_iter = struct.iter_unpack('ffff', content)
+        for idx, point in enumerate(pc_iter):
+            pc_list.append([point[0], point[1], point[2]])
+    return np.asarray(pc_list, dtype=np.float32)
+
+def load_label_kitti(label_path, remap_lut):
+    label = np.fromfile(label_path, dtype=np.uint32)
+    label = label.reshape((-1))
+    sem_label = label & 0xFFFF  # semantic label in lower half
+    inst_label = label >> 16  # instance id in upper half
+    assert ((sem_label + (inst_label << 16) == label).all())
+    sem_label = remap_lut[sem_label]
+    return sem_label.astype(np.int32)
+
+def get_labels(label_path, config):
+    remap_dict_val = config["learning_map"]
+    max_key = max(remap_dict_val.keys())
+    remap_lut_val = np.zeros((max_key + 100), dtype=np.int32)
+    remap_lut_val[list(remap_dict_val.keys())] = list(remap_dict_val.values())
+    labels = load_label_kitti(label_path, remap_lut=remap_lut_val)
+    return labels
+
+def read_calib_file(filepath):
+    """
+    Read in a calibration file and parse into a dictionary.
+    Ref: https://github.com/utiasSTARS/pykitti/blob/master/pykitti/utils.py
+    """
+    data = {}
+    with open(filepath, 'r') as f:
+        for line in f.readlines():
+            line = line.rstrip()
+            if len(line) == 0: continue
+            key, value = line.split(':', 1)
+            # The only non-float values in these files are dates, which
+            # we don't care about anyway
+            try:
+                data[key] = np.array([float(x) for x in value.split()])
+            except ValueError:
+                pass
+    return data
+
