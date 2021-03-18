ae660538665c99d2585960bd7d780307005ea504
Siddharth Baburaj <siddharth.baburaj@gmail.com>
2021-03-18 09:18:07 +0100

    Refactoring wrt model names, imports

--- model.py
+++ model.py
@@ -1,14 +1,12 @@
-import tensorflow as tf
-
 import os
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
 
 from tensorflow.keras.layers import Dropout, BatchNormalization, Input, Concatenate, Dense, Add
 from tensorflow.keras.models import Model
 from tensorflow.keras.regularizers import l2
-from spektral.layers import GCNConv, GlobalMaxPool, MinCutPool, GCSConv, EdgeConv, TopKPool
-from train_utils.tf_utils import unPool, GConv
+from spektral.layers import GCNConv
+from train_utils.layers import GConv
 
 
 def conv_relu_bn(parents, filters, dropout=False, l2_reg=0.01):
@@ -19,8 +17,7 @@ def conv_relu_bn(parents, filters, dropout=False, l2_reg=0.01):
         x = Dropout(0.2)(x)
     return x
 
-
-def Res_GCN_v7(tr_params):
+def Concat_GCN(tr_params):
 
     l2_reg = tr_params['l2_reg']
     F = tr_params['n_node_features']
@@ -51,17 +48,16 @@ def Res_GCN_v7(tr_params):
 
     output = GCNConv(num_classes, activation='softmax', name='gcn_6')([x, A_in])
 
-    model = Model(inputs=[X_in, A_in, I_in], outputs=output, name='Res_GCN_v7')
+    model = Model(inputs=[X_in, A_in, I_in], outputs=output, name='U-GCN4')
     return model
 
-def Res_GCN_nat(tr_params):
+def Concat_GCN_nat(tr_params):
 
     F = tr_params['n_node_features']
     num_classes = tr_params['num_classes']
 
     X_in = Input(shape=(F,), name='X_in')
     A_in = Input(shape=(None,), sparse=True)
-    I_in = Input(shape=(), name="segment_ids_in", dtype=tf.int32)
 
     levels = 4
 
@@ -83,157 +79,61 @@ def Res_GCN_nat(tr_params):
     x = Concatenate()([x, X_1])
 
     output = GConv(num_classes, activation='softmax', kernel_init='glorot_uniform')([x, A_in])
-    model = Model(inputs=[X_in, A_in, I_in], outputs=output, name='Res_GCN_v7')
+    model = Model(inputs=[X_in, A_in], outputs=output, name='Res_GCN_v7')
     return model
 
-def topk_gcn(tr_params):
+def Res_GCN(tr_params, levels=7):
 
-    l2_reg = tr_params['l2_reg']
     F = tr_params['n_node_features']
     num_classes = tr_params['num_classes']
 
     X_in = Input(shape=(F,), name='X_in')
     A_in = Input(shape=(None,), sparse=True)
-    I_in = Input(shape=(), name="segment_ids_in", dtype=tf.int32)
 
-    X_1 = conv_relu_bn((X_in, A_in), 16, l2_reg=l2_reg)
-    X_2, A_2, I_2 = TopKPool(0.5, kernel_regularizer=l2(l2_reg))([X_1, A_in, I_in])
+    skips = []
+    x, a = X_in, A_in
+    x = GConv(32)([x, a])
+    skips.append(x)
 
-    X_3 = conv_relu_bn((X_2, A_2), 16, l2_reg=l2_reg)
-    X_4, A_4, I_4 = TopKPool(0.5, kernel_regularizer=l2(l2_reg))([X_3, A_2, I_2])
+    for i in range(levels - 1):
+        x = GConv(32)([x, a])
+        x = Add()([x, skips[i]])
+        skips.append(x)
 
-    X_5 = conv_relu_bn((X_4, A_4), 16, l2_reg=l2_reg)
-    X_6 = conv_relu_bn((X_5, A_4), 16, l2_reg=l2_reg)
-    # X_6, A_6, I_6 = TopKPool(0.4, kernel_regularizer=l2(l2_reg))([X_5, A_4, I_4])
+    x = GConv(32)([x, a])
+    x = Concatenate()([x, *skips])
 
-    concat = Concatenate(axis=0)([X_6, X_5, X_3])
+    output = GConv(num_classes, activation='softmax', kernel_init='glorot_uniform')([x, A_in])
 
-    dense1 = mlp_block(concat, 64)
-    dense2 = mlp_block(dense1, 32)
-    output = Dense(num_classes, activation='softmax')(dense2)
-
-    model = Model(inputs=[X_in, A_in, I_in], outputs=output, name='GraphSEG_v4')
+    model = Model(inputs=[X_in, A_in], outputs=output, name='Res_GCN_v7')
 
     return model
 
-def edgeconv(parents, l2_reg, filters=32):
 
-    x, a = parents
-    output = EdgeConv(filters, aggregate='max', activation='relu',
-                      kernel_regularizer=l2(l2_reg), kernel_initializer='he_normal')([x, a])
-    output = BatchNormalization()(output)
+def Dense_GCN(tr_params, levels=7):
 
-    return output
-
-def mlp_block(input, units, do=True, bn=False):
-
-    output = Dense(units, activation='relu', kernel_initializer='he_normal')(input)
-    if do:
-        output = Dropout(0.3)(output)
-    if bn:
-        output = BatchNormalization()(output)
-
-    return output
-
-def dgcnn_v1(model_cfg):
-
-    l2_reg = model_cfg['l2_reg']
-    F = model_cfg['n_node_features']
-    num_classes = model_cfg['num_classes']
+    F = tr_params['n_node_features']
+    num_classes = tr_params['num_classes']
 
     X_in = Input(shape=(F,), name='X_in')
     A_in = Input(shape=(None,), sparse=True)
 
-    X_1 = edgeconv((X_in, A_in), l2_reg)
-    X_2 = edgeconv((X_1, A_in), l2_reg)
-    X_3 = edgeconv((X_2, A_in), l2_reg)
+    skips = []
+    x, a = X_in, A_in
+    x = GConv(32)([x, a])
+    skips.append(x)
 
-    concat_1 = Concatenate()([X_1, X_2, X_3])
+    for i in range(levels - 1):
+        x = GConv(32)([x, a])
+        x = Concatenate()([x, skips[i]])
+        skips.append(x)
 
-    mlp_1 = mlp_block(concat_1, 128, do=False, bn=True)
-    mlp_2 = mlp_block(mlp_1, 128, do=False, bn=True)
-    mlp_3 = mlp_block(mlp_2, 64, bn=True)
-    mlp_4 = mlp_block(mlp_3, 32, bn=True)
+    skips.pop()
+    x = GConv(32)([x, a])
+    x = Concatenate()([x, *skips])
 
-    output = Dense(num_classes, activation='softmax')(mlp_4)
+    output = GConv(num_classes, activation='softmax', kernel_init='glorot_uniform')([x, A_in])
 
-    model = Model(inputs=[X_in, A_in], outputs=output, name='EdgeConv_v1')
+    model = Model(inputs=[X_in, A_in], outputs=output, name='Dense_GCN')
+
     return model
-
-
-class Graph_U(Model):
-    def __init__(self, tr_params, verbose=False):
-        super(Graph_U, self).__init__()
-        self.l2_reg = tr_params['l2_reg']
-        self.F = tr_params['n_node_features']
-        self.num_classes = tr_params['num_classes']
-
-        self.num_levels = 3
-        self.v = verbose
-
-        self.x_agg = [None for i in range(2 * (self.num_levels))]
-        self.x_pool = [None for i in range(self.num_levels)]
-        self.a_pool = [None for i in range(self.num_levels)]
-        self.x_unpool = [None for i in range(self.num_levels)]
-        self.a_unpool = [None for i in range(self.num_levels)]
-        self.s_pool = [None for i in range(self.num_levels)]
-
-    def call(self, inputs):
-
-        self.autoencoder(parents=inputs)
-
-        softmax_inputs = [self.x_agg[-1], self.a_unpool[-1]]
-
-        output = GCNConv(self.num_classes, activation='softmax')(softmax_inputs)
-
-        return output
-
-    def autoencoder(self, parents):
-
-        X_in, A_in = parents
-        ds_cluster = round(X_in.shape[0] * 0.5)
-        if self.v:
-            print('Min_cut clustering size: ', ds_cluster)
-            print('X_in size: ', X_in.shape)
-            print('A_in size: ', A_in.shape)
-            print('-------')
-
-        for i in range(self.num_levels):
-            self.x_pool[i], self.a_pool[i], self.s_pool[i] = MinCutPool(k=ds_cluster, return_mask=True)([X_in, A_in])
-            if self.v:
-                print(' Downsampling level : ', i)
-                print('After Pooling:')
-                print('X_pool_{} size: {}'.format(i, self.x_pool[i].shape))
-                print('A_pool_{} size: {}'.format(i, self.a_pool[i].shape))
-                print('S_pool_{} size: {}'.format(i, self.s_pool[i].shape))
-            self.x_agg[i] = self.gcn_block(inputs=(self.x_pool[i], self.a_pool[i]))
-            if self.v:
-                print('After Aggregation:')
-                print('X_Agg_{} size: {}'.format(i, self.x_agg[i].shape))
-                print('-------')
-            ds_cluster = round(self.x_agg[i].shape[0] * 0.5)
-
-        for j in range(self.num_levels):
-            k = self.num_levels - j - 1
-            self.x_unpool[j], self.a_unpool[j] = unPool()([self.x_agg[k], self.a_pool[k], self.s_pool[k]])
-            if self.v:
-                print(' Upsampling level : ', j)
-                print('After Unpooling:')
-                print('X_unpool_{} size: {}'.format(j, self.x_unpool[j].shape))
-                print('A_unpool_{} size: {}'.format(j, self.a_unpool[j].shape))
-            self.x_agg[j + self.num_levels] = self.gcn_block(inputs=(self.x_unpool[j], self.a_unpool[j]), dropout=True)
-            if self.v:
-                print('After Aggregation:')
-                print('X_Agg_{} size: {}'.format(j + self.num_levels, self.x_agg[j + self.num_levels].shape))
-                print('-------')
-
-    def gcn_block(self, inputs, filters=32, dropout=False):
-        x, a = inputs
-
-        x = GCNConv(filters, activation='relu', kernel_regularizer=l2(self.l2_reg))([x, a])
-        x = BatchNormalization()(x)
-        if dropout:
-            x = Dropout(0.1)(x)
-
-        return x
-
