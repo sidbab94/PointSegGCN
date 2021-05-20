cb48d7f99809d15a7abd0cdae43746992af2caee
Siddharth Baburaj <siddharth.baburaj@gmail.com>
2021-05-20 19:45:51 +0200

    removed old spektral models, added dropout to curr

--- train_utils/models.py
+++ train_utils/models.py
@@ -1,54 +1,9 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-import tensorflow as tf
 
 from tensorflow.keras.layers import Dropout, Input, Concatenate, Add
 from tensorflow.keras.models import Model
-from tensorflow.keras.regularizers import l2
 from train_utils.layers import GConv, CRF
-
-
-def conv_relu_bn(parents, filters, dropout=False, l2_reg=0.01):
-    X_in, A_in = parents
-    x = GCNConv(filters, activation='relu', kernel_initializer='he_normal', kernel_regularizer=l2(l2_reg))([X_in, A_in])
-    # x = BatchNormalization()(x)
-    if dropout:
-        x = Dropout(0.2)(x)
-    return x
-
-def Concat_GCN(tr_params):
-
-    l2_reg = tr_params['l2_reg']
-    F = tr_params['n_node_features']
-    num_classes = tr_params['num_classes']
-
-    X_in = Input(shape=(F,), name='X_in')
-    A_in = Input(shape=(None,), sparse=True)
-    I_in = Input(shape=(), name="segment_ids_in", dtype=tf.int32)
-
-    levels = 4
-
-    skips = []
-
-    x = GCNConv(32, activation='relu', kernel_regularizer=l2(l2_reg))([X_in, A_in])
-    X_1 = x
-
-    for i in range(levels):
-        x = conv_relu_bn((x, A_in), 32, False)
-        skips.append(x)
-
-    skips = reversed(skips)
-
-    for skip in skips:
-        x = conv_relu_bn((x, A_in), 32, False)
-        x = Concatenate()([x, skip])
-
-    x = Concatenate()([x, X_1])
-
-    output = GCNConv(num_classes, activation='softmax', name='gcn_6')([x, A_in])
-
-    model = Model(inputs=[X_in, A_in, I_in], outputs=output, name='U-GCN4')
-    return model
 
 def Concat_GCN_nat(tr_params):
 
@@ -124,6 +79,7 @@ def Dense_GCN(tr_params, levels=7):
 
     for i in range(levels - 1):
         x = GConv(32)([x, a])
+        x = Dropout(0.2)(x)
         x = Concatenate()([x, skips[i]])
         skips.append(x)
 
